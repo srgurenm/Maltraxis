0a1305a562b5fbbe5e3b7d2084358856c26e069a
srgurenm <jcsierrah77@gmail.com>
2026-06-09 14:32:19 -0500

    Change image fit to object-contain for full book cover visibility

--- src/App.jsx
+++ src/App.jsx
@@ -404,7 +404,7 @@ export default function App() {
             {/* Book 1 */}
             <div className="bg-black/60 rounded-2xl p-6 border border-white/5 hover:border-red-900/20 transition-all flex flex-col sm:flex-row gap-6">
               <a href="https://www.amazon.com/Maltraxis-ciudad-las-sombras-Spanish-ebook/dp/B0F77932D6" target="_blank" rel="noopener noreferrer" className="w-full sm:w-1/3 aspect-[2/3] overflow-hidden rounded-lg relative block">
-                <img src="/portada.jpg" alt="Libro 1" className="w-full h-full object-cover hover:scale-105 transition-transform duration-500" />
+                <img src="/portada.jpg" alt="Libro 1" className="w-full h-full object-contain bg-black hover:scale-105 transition-transform duration-500" />
               </a>
               <div className="flex-1 flex flex-col justify-between">
                 <div>
@@ -429,7 +429,7 @@ export default function App() {
             {/* Book 2 */}
             <div className="bg-black/60 rounded-2xl p-6 border border-white/5 hover:border-amber-500/20 transition-all flex flex-col sm:flex-row gap-6">
               <div className="w-full sm:w-1/3 aspect-[2/3] overflow-hidden rounded-lg relative">
-                <img src="/portada2.png" alt="Libro 2" className="w-full h-full object-cover" />
+                <img src="/portada2.png" alt="Libro 2" className="w-full h-full object-contain bg-black" />
               </div>
               <div className="flex-1 flex flex-col justify-between">
                 <div>
